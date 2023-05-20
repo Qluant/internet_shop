703c7ec9b2a7581e970ea12fb4207e9412c785eb
Acutapugione <acuta.pugione@gmail.com>
2023-05-20 12:55:05 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    DATABASE='internet_shop'
+    USER='acuta'
+    PASSWORD='acuta'
+    HOST='127.0.0.1'
+    PORT='5432'
+    API_TOKEN='6153100291:AAFf5zMqonJVtW_wVPmwvWi34zMwdeafRtc'
