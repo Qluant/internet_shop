07d0365b7ff5d888153de573fee704ee304aec4d
Acutapugione <acuta.pugione@gmail.com>
2023-05-27 12:31:31 +0300

    initial

--- bot.py
+++ bot.py
@@ -1,48 +1,5 @@
-import logging
-from aiogram import Bot, Dispatcher, executor, types
-from settings import Settings
-import asyncio
-import sys
-import uvloop
-from main import DB_Worker
-
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=Settings.API_TOKEN)
-dp = Dispatcher(bot)
-
-# # Додавання запису
-# with DB_Worker() as db:
-#         db.insert('countries', {'title' : 'USA'})
-
-@dp.callback_query_handler(lambda query: query.data == 'add_country')
-async def add_country(call: types.CallbackQuery):
-    with DB_Worker() as db:
-        db.insert('countries', {'title' : 'USA'})
-    await call.message.answer('Done')
-
-
-@dp.message_handler(commands=["start", "help"])
-async def send_welcome(message: types.Message):
-    markup = types.InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                types.InlineKeyboardButton("Add country", callback_data="add_country"),
-            ],
-        ],
-    )
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.", reply_markup=markup)
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
-
-    await message.answer(message.text)
-
+from bot import executor, dp
+import controllers
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
+from vendor.config import Config
+
+
 class Settings:
-    DATABASE='internet_shop'
-    USER='acuta'
-    PASSWORD='acuta'
-    HOST='127.0.0.1'
-    PORT='5432'
-    API_TOKEN='6153100291:AAFf5zMqonJVtW_wVPmwvWi34zMwdeafRtc'
+    DATABASE=Config.DATABASE
+    API_TOKEN=Config.API_TOKEN
